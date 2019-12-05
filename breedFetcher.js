2c14e4364050dce57f54fa1fef41a7b897d0ab74
alexfalco <afalconer02@gmail.com>
2019-12-05 17:17:39 -0500

    refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,33 +1,35 @@
 const request = require('request');
+//const breedName = require('./index');
 
-
-let name = process.argv.slice(2).toString();
-console.log(`name of dog: ${name}`);
-
-
-let website = `https://api.thecatapi.com/v1/breeds/search?name=${name}`;
-
-
-request(website, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
   
-  if (error) {
-    console.log('login error:',error);
-  }
-    
-
-  const data = JSON.parse(body);
-  
-  try {
-    console.log(data[0].description);
-  }
-  
-  catch (err) {
-    console.log('error: name not found ');
-  }
-
+  let website = `https://api.thecatapi.com/v1/breeds/search?name=${breedName}`;
  
-
+  request(website, (error, response, body) => {
+    
+    if (error) {
+      callback(error, null);
+    }
+      
+    const data = JSON.parse(body);
+    
+    try {
+      callback(null,data[0].description.trim());
+    }
+    
+    catch (err) {
+      callback(err,null);
+    }
   
+  });
 
-});
+};
+
+
+module.exports = { fetchBreedDescription };
+
+
+
+
+
 
